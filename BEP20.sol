2a14b5925a9d85fd7c0932fb4615e12013d1cf49
OpenZeppelinn <101998352+OpenZeppelinn@users.noreply.github.com>
2022-03-21 05:52:19 +0700

    Update BEP20.sol

--- BEP20.sol
+++ BEP20.sol
@@ -1,7 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-import "./IBEP20.sol";
+interface IBEP20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+interface IBEP20Metadata is IBEP20 {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+}
 
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
